a3ad54d7d3de698f599e5781d68770aa9e211956
Muhammad Vohra <Muhammed.vohra.work@gmail.com>
2019-12-04 20:05:09 +0000

    made wordpress boilerplate

---
+++ README.md
@@ -0,0 +1,19 @@
+# Untitled Wordpress Boilerplate plugin
+
+Simple library to speed up the process of building post types, removing unwanted headers etc. This is pulled in as a package and the `Boilerplate::bootstrap();` method should be run at the **end** of your `functions.php` file.
+
+## Usage
+
+Simply pull in as a package through composer
+
+```
+...
+"require": {
+    ""untitled/wordpress-boilerplate": "~1.0"
+}
+...
+```
+
+TODO:
+
+* Write documentation on all of the features and how to use them.
---
+++ src/Boilerplate.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Uncode\Wordpress\Boilerplate;
+
+use Uncode\Wordpress\Boilerplate\Core\Wordpress\DisableXmlRpc;
+use Uncode\Wordpress\Boilerplate\Core\Wordpress\RemoveEmojiScript;
+use Uncode\Wordpress\Boilerplate\Core\Wordpress\RemoveGenerator;
+use Uncode\Wordpress\Boilerplate\Core\Wordpress\RemovePingback;
+use Uncode\Wordpress\Boilerplate\Core\Wordpress\RemoveRsdLink;
+use Uncode\Wordpress\Boilerplate\Core\Wordpress\RemoveShortLink;
+use Uncode\Wordpress\Boilerplate\Core\Wordpress\RemoveWlwManifest;
+use Uncode\Wordpress\Boilerplate\Core\Timber\Defaults as TimberDefaults;
+
+class Boilerplate
+{
+    /**
+     * Start the boilerplate bootstrap process.
+     */
+    public static function bootstrap()
+    {
+        DisableXmlRpc::run();
+        RemoveEmojiScript::run();
+        RemoveGenerator::run();
+        RemovePingback::run();
+        RemoveRsdLink::run();
+        RemoveShortLink::run();
+        RemoveWlwManifest::run();
+        TimberDefaults::run();
+    }
+}
